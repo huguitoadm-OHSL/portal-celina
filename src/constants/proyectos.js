02dea10d48babc1538f83a18c82e4f55e7ea2674
huguitoadm-OHSL <huguitoadm@gmail.com>
2026-06-12 23:25:33 -0400

    Create proyectos.js

---
+++ src/constants/proyectos.js
@@ -0,0 +1,22 @@
+// --- CONFIGURACIÓN DE PROYECTOS ---
+export const PROYECTOS_CONVENIO_1 = ["Los Jardines", "El Renacer", "Rancho Nuevo", "Santa Fe"];
+export const PROYECTOS_CONVENIO_2 = ["Cañaveral"];
+export const PROYECTOS_PROPIOS_1 = ["Muyurina"];
+
+export const PROYECTOS = [
+  "Cañaveral", 
+  "El Renacer", 
+  "Los Jardines", 
+  "Muyurina", 
+  "Rancho Nuevo", 
+  "Santa Fe", 
+  "OTRO..."
+];
+
+export const NOMBRES_PROYECTOS_PROYECCION = [
+  "Muyurina", 
+  "Renacer", 
+  "Santa Fe", 
+  "Rancho Nuevo", 
+  "Jardines"
+];
