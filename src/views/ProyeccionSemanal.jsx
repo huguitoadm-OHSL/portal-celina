6cb46a1ba5b2c3b84183d7b5f9960feb74c88904
huguitoadm-OHSL <huguitoadm@gmail.com>
2026-06-14 15:29:31 -0400

    Update ProyeccionSemanal.jsx

--- src/views/ProyeccionSemanal.jsx
+++ src/views/ProyeccionSemanal.jsx
@@ -93,7 +93,7 @@ export default function ProyeccionSemanal() {
   const inyectarHistorialMaestro = async () => {
     // Array estricto con los 6 proyectos: [Muyurina, Renacer, Santa Fe, Rancho Nuevo, Jardines, Celina VII]
     const asesoresHistorial = [
-      { nombre: "MARISOL URGEL PIZARRO", colAct: 24984, dias: [0,0,0,0,0,0,0], proy: [0,0,0,0,0,0], ventasReales: [1,0,0,0,0,0] }, 
+      { nombre: "MARISOL URGEL PIZARRO", colAct: 24384, dias: [0,0,0,0,0,0,0], proy: [0,0,0,0,0,0], ventasReales: [1,0,0,0,0,0] }, 
       { nombre: "CARLOS ENRIQUE CALDERON", colAct: 0, dias: [0,0,0,0,0,0,0], proy: [0,0,0,0,0,0], ventasReales: [0,0,0,0,0,0] },
       { nombre: "ELY GONZALES GARCIA", colAct: 7200, dias: [0,0,0,0,0,0,0], proy: [0,0,0,0,0,0], ventasReales: [0,0,0,0,0,1] }, 
       { nombre: "RODRIGO ROJAS SILES", colAct: 0, dias: [0,0,0,0,0,0,0], proy: [0,0,0,0,0,0], ventasReales: [0,0,0,0,0,0] },
