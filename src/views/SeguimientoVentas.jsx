594c929e00cf0ab0fa9f96254da49784500df04d
Oscar H. Saravia <huguitoadm@gmail.com>
2026-09-07 23:40:21 -0400

    Update SeguimientoVentas.jsx

--- src/views/SeguimientoVentas.jsx
+++ src/views/SeguimientoVentas.jsx
@@ -16,7 +16,7 @@ const BASE_DE_DATOS_PBI = [
 ];
 
 export default function SeguimientoVentas() {
-  const ventasPorProyecto = [0, 0, 0, 0, 0, 0, 0]; 
+  const ventasPorProyecto = [0, 0, 0, 0, 5, 0, 0]; 
 
   const datosProcesados = BASE_DE_DATOS_PBI.map(asesor => {
     let totalVentas = 0;
