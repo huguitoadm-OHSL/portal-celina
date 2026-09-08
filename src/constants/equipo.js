0539c098f48d577d5d936d134abd122bd88ad6d2
Oscar H. Saravia <huguitoadm@gmail.com>
2026-09-07 22:51:15 -0400

    Update equipo.js

--- src/constants/equipo.js
+++ src/constants/equipo.js
@@ -18,7 +18,7 @@ export const EQUIPOS_ASESORES = {
     { nombre: "Marisol Urgel Pizarro", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Carlos Enrique Calderon", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Ely Gonzales Garcia", colAct: 0, tipo: "Interno", ventas: 0 },
-    { nombre: "Jimmy Gonzales Nuñez", colAct: 37944, tipo: "Interno", ventas: 5 },
+    { nombre: "Jimmy Gonzales Nuñez", colAct: 37.944, tipo: "Interno", ventas: 5 },
     { nombre: "Jaime F. Rios Castro", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Merly Mendez Hurtado", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Jose Gabriel Padilla Loayza", colAct: 0, tipo: "Interno", ventas: 0 },
@@ -27,5 +27,3 @@ export const EQUIPOS_ASESORES = {
 export const OBJETIVOS_MENSUALES = {
   "Oscar Saravia": 115000 
 };
-
-
