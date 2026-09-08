45e28d6cd6cccb9ebe8d0e2170f2fbd11023154c
Oscar H. Saravia <huguitoadm@gmail.com>
2026-09-07 22:52:16 -0400

    Update equipo.js

--- src/constants/equipo.js
+++ src/constants/equipo.js
@@ -18,7 +18,7 @@ export const EQUIPOS_ASESORES = {
     { nombre: "Marisol Urgel Pizarro", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Carlos Enrique Calderon", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Ely Gonzales Garcia", colAct: 0, tipo: "Interno", ventas: 0 },
-    { nombre: "Jimmy Gonzales Nuñez", colAct: 37.944, tipo: "Interno", ventas: 5 },
+    { nombre: "Jimmy Gonzales Nuñez", colAct: 37944.00, tipo: "Interno", ventas: 5 },
     { nombre: "Jaime F. Rios Castro", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Merly Mendez Hurtado", colAct: 0, tipo: "Interno", ventas: 0 },
     { nombre: "Jose Gabriel Padilla Loayza", colAct: 0, tipo: "Interno", ventas: 0 },
